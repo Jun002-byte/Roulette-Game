395c308d056ff62f038f33565be1d8dad0b5a89b
Robert <77898045+remken07@users.noreply.github.com>
2025-01-08 11:27:02 -0500

    user thing

--- Roulette_Game.py
+++ Roulette_Game.py
@@ -12,10 +12,12 @@ currentSpot = int()
 def main():
   while chips > 0:
     #ask for bet/number of chips
-
+    print(f"You have {chips} chips")
+    betSpot = input("Enter where you would like to be one: ")
+    betAmount = input("Enter the amount of chips: ")
     
     currentSpot = random.randint(0,36) #36 is actually 00
-    print(f"Spot landed on is {currentSpot}, you [enter win or lose here and amount]")
+    print(f"You bet on {betSpot}, for {betAmount} chips\nSpot landed on is {currentSpot}, you [enter win or lose here and amount]")
 
     #deal with adding or removing chips here
 
