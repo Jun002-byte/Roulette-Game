3396712a98ea8acbcbd296c271648c6af45ebdfc
Jun002-byte <114766306+Jun002-byte@users.noreply.github.com>
2025-01-08 11:43:02 -0500

    Update Roulette_Game.py

--- Roulette_Game.py
+++ Roulette_Game.py
@@ -6,18 +6,30 @@ board = []
 #set up scoring data here/list of all possible bet spots which can be printed out later
 
 
-chips = int(20)
-currentSpot = int()
+
 
 def main():
+  chips = int(20)
+  currentSpot = int()
+
+
   while chips > 0:
     #ask for bet/number of chips
     print(f"You have {chips} chips")
-    betSpot = input("Enter where you would like to be one: ")
-    betAmount = input("Enter the amount of chips: ")
+    betSpot = int(input("Enter where you would like to be one: "))
+    betAmount = int(input("Enter the amount of chips: "))
+
     
     currentSpot = random.randint(0,36) #36 is actually 00
-    print(f"You bet on {betSpot}, for {betAmount} chips\nSpot landed on is {currentSpot}, you [enter win or lose here and amount]")
+    if currentSpot == 36:
+      print(f"Spot landed on is 00 ")
+    print(f"Spot landed on is {currentSpot}")
+    if betSpot == currentSpot:
+      winAmount = betAmount * 35
+      print(f"You win {winAmount}!")
+      chips += winAmount
+    
+    
 
     #deal with adding or removing chips here
 
